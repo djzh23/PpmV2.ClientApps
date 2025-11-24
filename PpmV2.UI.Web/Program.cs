f6a3d58debb77c60a2787a9daece2502e711a02c
djzh23 <zouh.ijd@gmail.com>
2025-11-24 15:26:22 +0100

    Add shared login UI and integrate RCL routing

---
+++ PpmV2.UI.Web/Program.cs
@@ -0,0 +1,31 @@
+using PpmV2.UI.Web.Components;
+using PpmV2.UI.Shared;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // Serves files from wwwroot folder
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(new[] { typeof(SharedAssemblyMarker).Assembly });
+
+app.Run();

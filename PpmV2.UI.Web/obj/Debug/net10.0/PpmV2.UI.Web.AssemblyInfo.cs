76ec3081c75a3ba8c1ee5ca09e937bd1a8438884
djzh23 <zouh.ijd@gmail.com>
2025-11-30 04:37:55 +0100

    feat: remove role for updated registration flow

--- PpmV2.UI.Web/obj/Debug/net10.0/PpmV2.UI.Web.AssemblyInfo.cs
+++ PpmV2.UI.Web/obj/Debug/net10.0/PpmV2.UI.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PpmV2.UI.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e39e40adbd583eb26b26f20c42f5a8cd25b627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72b225132a2dbba94e5abf6f17d7326691d5ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PpmV2.UI.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PpmV2.UI.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
